27a74fb1013181ab27e0320277e60fe4383d36a3
Drew <drwomble27@gmail.com>
2023-03-29 16:47:33 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let names = ["Maggie", "Drew", "Olive"];
+
+function writeCards(names){
+    let cards = [];
+    for(let a = 0; a < names.length; a++){
+        cards[a] = (`Thank you, ${names[a]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+writeCards(names);
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number--);
+    }
+    return countDown;
+}
